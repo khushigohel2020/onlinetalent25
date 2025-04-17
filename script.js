291dbdacba3ac0c324ce08d6444ffa8b6783c2af
khushigohel2020 <khushigohel1627@gmail.com>
2025-04-17 09:55:18 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+function startp2() {
+    window.location.href = "p2.html";
+  }
+
+function startp3() {
+    window.location.href = "P2-2.html";
+  }  
+
+  function startp4() {
+    window.location.href = "p2-3.html";
+  }  
+  
+  function startp5() {
+    window.location.href = "p2-4.html";           
+  }  
+  function startp6() {
+    window.location.href = "p2-5.html";           
+  }  
+  
+  function markComplete() {
+    localStorage.setItem("courseCompleted", "yes");
+    alert("Course marked as complete!");
+  }
+  
+ 
